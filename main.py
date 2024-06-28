9f84c3314f88940145b236c5f7b392f778bdb5eb
Nikita <nikpotoo@gmail.com>
2024-06-28 03:18:06 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""TODO: Надо сделать красивое главное меню"""
+
+
+
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
